645fb770e43ce874bbf9b5207d6c701b2740ef2e
Tobias Dühr <nerd@tobiasduehr.com>
2019-04-03 23:19:16 +0200

    server side callback after upload - WIP

--- scripts/callback.php
+++ scripts/callback.php
@@ -1,1 +1,26 @@
 <?php
+
+$payload = json_decode(file_get_contents('php://input'));
+header('content-type: application/json');
+
+switch ($payload->action){
+    case 'ping':
+        echo json_encode(['pong' => true]);
+    break;
+    case 'decompress':
+        $zip = new ZipArchive;
+        $res = $zip->open($payload->file);
+        if ($res === true) {
+            $zip->extractTo(dirname($payload->file));
+            $zip->close();
+            echo json_encode(['success' => true]);
+        } else {
+            echo json_encode(['error' => 'decompress_failed']);
+        }
+        unlink($payload->file);
+    break;
+    case 'self-destruct':
+        unlink(__FILE__);
+        exit;
+    break;
+}
